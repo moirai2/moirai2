696ea7975195f8bb1888815b3fcdecdd162f41e0
ah3q <akira.hasegawa@riken.jp>
2020-07-29 12:42:02 +0900

    update on 20200729.124202

--- README.md
+++ README.md
@@ -8,6 +8,19 @@ Input, output, parameter, time, stdout, stderr are recorded in RDF sqlite3 datab
 
 bash2cwl has moved to https://github.com/moirai2/bash2cwl
 
+
+## Structure
+```
+moirai2/
+├── commandeditor.html - HTML for editing command json.
+├── commandviewer.html - Viewintg a command json.
+├── js/ - Javascript used for MOIRAI2 manipulation through a browser.
+├── moirai2.php - Used for MOIRAI2 manipulation through a browser.
+├── moirai2.pl - Computes MOIRAI2 commands/network.
+├── rdf.pl - Script to handle Resource Description Framework (RDF) using SQLite3 database.
+└── README.md - This readme file
+```
+
 ## Install
 
 To install moirai2 to a directory named "project".
@@ -18,18 +31,372 @@ $ git clone https://github.com/moirai2/moirai2.git project
 
 ## Usage
 
+* To sort a file using a <a href="https://moirai2.github.io/command/text/sort.json">sort.json</a>.
+* Input will be prompted, so enter a filepath and push return.
+* After computation, sorted file will be output to work/sort.XXXXXXXXX/sort.txt(XXXXXXXXX is random).
+```shell
+$ perl moirai2.pl https://moirai2.github.io/command/text/sort.json
+#Input: input? input.txt
+```
+
+* A command json file used in the previous usage, specifies command lines, input, and output.
+* I have prepared basic command lines at <a href="https://moirai2.github.io/command">https://moirai2.github.io/command</a>.
+```shell
+{
+"https://moirai2.github.io/schema/daemon/input":"$input",
+"https://moirai2.github.io/schema/daemon/bash":["output=\"$workdir/sort.txt\"","sort $input > $output"],
+"https://moirai2.github.io/schema/daemon/output":"$output"
+}
+```
+
+* Path to input file can be assigned through argument, so you can skip prompt part.
+```shell
+$ perl moirai2.pl https://moirai2.github.io/command/text/sort.json input.txt
+```
+
+* By assigning output path, result will not be output to temporary file, but instead location can be specified.
+```shell
+$ perl moirai2.pl https://moirai2.github.io/command/text/sort.json input.txt output.txt
+```
+
+* In any cases, executed information will be stored in the Resource Description Framework (RDF) database.
+* You can view the database by rdf.pl.
+* Default database path is ./rdf.sqlite3.
+* All information are store din RDF triplet (subject, predicate, object).
+```shell
+$ perl rdf.pl select
+rdf.sqlite3#node1	https://moirai2.github.io/schema/daemon/command	https://moirai2.github.io/command/text/sort.json
+rdf.sqlite3#node1	https://moirai2.github.io/command/text/sort.json#output	ouput.txt
+rdf.sqlite3#node1	https://moirai2.github.io/command/text/sort.json#input	input/sn/sn_RNA_1.fa
+rdf.sqlite3#node1	https://moirai2.github.io/schema/daemon/timethrown	1595992099
+rdf.sqlite3#node1	https://moirai2.github.io/schema/daemon/timestarted	1595992099
+rdf.sqlite3#node1	https://moirai2.github.io/schema/daemon/timeended	1595992099
+```
+
+* To see the information about the command JSON, use help option.
+* Order of input/output are specified in '#Command' line.
+```shell
+$ perl moirai2.pl -h https://moirai2.github.io/command/text/sort.json
+
+#URL     :https://moirai2.github.io/command/text/sort.json
+#Command :sort.json [input] [output]
+#Input   :input
+#Output  :output
+#Bash    :output="$workdir/sort.txt"
+         :sort $input > $output
+```
+
+* To run command lines from data store in the RDF database.
+
+
+* The command lines don't have to be specified through json.
+```shell
+$ perl moirai2.pl -i 'A->B->$file' -o '$file->C->$result' exec << 'EOS'
+result=wc/${file.basename}.txt
+wc -l < $file > $result
+EOS
+```
+
+### moirai2.pl
+
 ```shell
 $ perl moirai2.pl CMDURL ARGV [ARGV ..]
 ```
 List of default commands are here:
 https://moirai2.github.io/command
 
-## Directory
+### rdf.pl
 
-js/ - Javascript used for MOIRAI2 manipulation through a browser.
-moirai2.php - Used for MOIRAI2 manipulation through a browser.
-moirai2.pl - Computes MOIRAI2 commands/network.
-rdf.pl - Script to handle Resource Description Framework (RDF) using SQLite3 database.
+#### select
+```shell
+rdf.pl -d DB select SUB PRE OBJ
+```
+* Select database subject, predicate, object.
+* Use '%' for a wildcard.
+```shell
+A->B->C
+
+> rdf.pl -d DB select A B C
+A B C
+> rdf.pl -d DB select A B %
+A B C
+> rdf.pl -d DB select % % C
+A B C
+```
+
+#### insert
+```shell
+rdf.pl -d DB insert SUB PRE OBJ
+```
+* Insert a new RDF (subject, predicate, object).
+```shell
+> rdf.pl -d DB insert D E F
+inserted 1
+```
+
+#### update
+```shell
+rdf.pl -d DB update SUB PRE OBJ
+```
+* Update/replace new object with defined subject and predicate.
+```shell
+A->B->C
+
+>rdf.pl -d DB update A B D
+updated 1
+
+A->B->D
+```
+
+#### delete
+```shell
+rdf.pl -d DB delete SUB PRE OBJ
+```
+* Delete RDF with defined subject, predicate, and object.
+```shell
+A->B->C
+
+>rdf.pl -d DB delete A B C
+deleted 1
+```
+
+#### object
+```shell
+rdf.pl -d DB object SUB PRE OBJ > VARIABLE
+```
+* Print out object with specified subject, predicate, and object.
+* If there are multiple objects, results will be printed out in one line with a space.
+```shell
+A->B->C
+A->B->D
+
+>rdf.pl -d DB object A B
+C D
+```
+
+#### network
+```shell
+rdf.pl -d DB network > TSV
+```
+* Print all RDF other than system RDF.
+```shell
+A->B->C
+A->B->D
+
+> rdf.pl -d DB network
+A   B   C
+A   B   D
+```
+
+#### network
+```shell
+rdf.pl -d DB import < TSV
+```
+* Import RDF from TSV.
+```shell
+> tsv.txt
+A   B   C
+D   E   F
+
+>rdf.pl -d DB import < tsv.txt
+A->B->C
+D->E->F
+```
+
+#### dump
+```shell
+rdf.pl -d DB dump > TSV
+```
+* Dump database in TSV format.
+```shell
+A->B->C
+D->E->F
+
+>rdf.pl -d DB dump > TSV
+A   B   C
+D   E   F
+```
+* Dump database in JSON format.
+```shell
+rdf.pl -d DB -f json dump > JSON
+```
+
+#### drop
+* Delete the table
+```shell
+rdf.pl -d DB drop
+```
+
+#### query
+```shell
+rdf.pl -d DB query QUERY > JSON
+```
+* Get key-val from database with a query.
+```shell
+A   B   C
+
+>perl rdf.pl query 'A->B->$c'
+[{"c":"C"}]
+```
+
+#### replace
+```shell
+rdf.pl -d DB replace FROM TO
+```
+* Replace node with a new value.
+```shell
+A   B   C
+
+rdf.pl -d DB replace C D
+replaced
+
+A   B   D
+```
+
+#### mv
+```shell
+rdf.pl -d DB mv FROM TO
+```
+* Replace variable in database and also move file to a new path.
+```shell
+A B C.txt
+
+rdf.pl -d DB mv C.txt D.txt
+
+A B D.txt
+move C.txt to D.txt
+```
+
+#### rm
+```shell
+rdf.pl -d DB rm PATH
+```
+* Remove a file and record.
+```shell
+A B C.txt
+
+rdf.pl -d DB rm C.txt
+
+C.txt	https://moirai2.github.io/schema/file/timeremoved	1595086775
+remove C.txt
+```
+
+#### newnode
+```shell
+rdf.pl -d DB newnode > NODE
+```
+# Create a new node ID.
+```shell
+> rdf.pl -d DB newnode
+rdf.sqlite3#node1
+```
+
+#### reindex
+```shell
+rdf.pl -d DB reindex
+```
+* Reindex database.
+
+#### download
+```shell
+rdf.pl -d DB download URL
+```
+* Download a file path and record information.
+
+#### command
+```shell
+rdf.pl -d DB -f json command < JSON
+```
+* Register execution of a command in the database.
+* Run a command with moirai2.pl.
+
+#### merge
+```shell
+rdf.pl -d DB merge DB2 DB3
+```
+* Merge database and database arguments.
+
+#### linecount
+```shell
+rdf.pl linecount DIR/FILE > TSV
+```
+* Count lines and register number.
+
+#### seqcount
+```shell
+rdf.pl seqcount DIR/FILE > TSV
+```
+* Count sequences and register number.
+
+#### filesize
+```shell
+rdf.pl filesize DIR/FILE > TSV
+```
+* Check file size and register.
+
+#### md5
+```shell
+rdf.pl md5 DIR/FILE > TSV
+```
+* Check md5 and register.
+
+#### ls
+```shell
+rdf.pl ls DIR/FILE > LIST
+```
+* List directory and register database.
+
+```shell
+rdf.pl -d DB ls '-' < STDIN
+```
+
+#### copy
+```shell
+rdf.pl -d DB -D DB2 copy QUERY
+```
+* Copy database to a new database with query.
+
+#### install
+```shell
+rdf.pl -d DB install URL
+```
+* Install software URL.
+
+#### rmexec
+```shell
+rdf.pl -d DB rmexec
+```
+* Remove currently running executes.
+
+#### input
+```shell
+rdf.pl -d DB input SUB PRE OBJECT OBJECT2 [..]
+```
+* Input multiple objects.
+
+#### prompt
+```shell
+rdf.pl -d DB prompt SUB PRE QUESTION DEFAULT
+```
+* Prompt and insert database.
+
+#### executes
+```shell
+rdf.pl -d DB executes
+```
+* Output execute information.
+
+#### html
+```shell
+rdf.pl -d DB html
+```
+* Output execute information in html mode.
+
+#### history
+```shell
+rdf.pl -d DB history
+```
+* Output execute information in shell mode.
 
 ## Licence
 
--- js/ah3q/RDF.js
+++ js/ah3q/RDF.js
@@ -1,12 +1,43 @@
 //#################### constructor ####################
-function RDF(){this.rdf={};return this;}
+function RDF(){
+	this.parent;//parent of RDF
+	this.rdf={};//triplets
+	this.results=[];//results from query()
+	this.returns=[];//results from execute()
+	this.mode=0;//0=rdf,1=query(),2=execute()
+	return this;
+}
 //#################### add ####################
 RDF.prototype.add=function(subject,predicate,object){
+	this.mode=0;
 	if(!(subject in this.rdf))this.rdf[subject]={};
 	if(!(predicate in this.rdf[subject]))this.rdf[subject][predicate]=object;
 	else if(Array.isArray(this.rdf[subject][predicate])){
 		if(!this.rdf[subject][predicate].contains(object))this.rdf[subject][predicate].push(object);
 	}else if(this.rdf[subject][predicate]!=object)this.rdf[subject][predicate]=[this.rdf[subject][predicate],object];
+	return this;
+}
+//#################### build ####################
+RDF.prototype.build=function(string){
+	if(string==null)return this;
+	let keys=this._keys();
+	if(!Array.isArray(string))string=string.split(",");
+	let inserts=[];
+	for(let i=0;i<string.length;i++){
+		for(let j=0;j<this.returns.length;j++){
+			let line=string[i];
+			let tokens=line.split("->");
+			if(tokens.length!=3)continue;
+			for(let k=0;k<keys.length;k++){
+				let key=keys[k]
+				let val=this.results[j][key];
+				line=line.replace(key,val);
+			}
+			line=line.replace("$_",this.returns[j]);
+			inserts.push(line);
+		}
+	}
+	this.insert(inserts);
 	return this;
 }
 //#################### delete ####################
@@ -23,59 +54,47 @@ RDF.prototype.delete=function(subject,predicate,object){
 		delete(this.rdf[subject][predicate]);
 		if(Object.keys(this.rdf[subject]).length==0)delete(this.rdf[subject]);
 	}
+	this.mode=0;
 	return this;
 }
-//#################### html ####################
-RDF.prototype.toHTML=function(){
-	let html=$("<table/>");
-		let thead=$("<thead/>").append($("<tr/>").append($("<th>").text("subject")).append($("<th>").text("predicate")).append($("<th>").text("object")));
-	html.append(thead);
-	let tbody=$("<tbody/>");
-	html.append(tbody);
+//#################### exclude ####################
+RDF.prototype.exclude=function(subject,predicate,object){
+	let rdf=new RDF();
 	let subjects=Object.keys(this.rdf);
 	for(let i=0;i<subjects.length;i++){
-		let subject=subjects[i];
-		let predicates=Object.keys(this.rdf[subject]);
+		let subject2=subjects[i];
+		if(this._exclude(subject,subject2))continue;
+		let predicates=Object.keys(this.rdf[subject2]);
 		for(let j=0;j<predicates.length;j++){
-			let predicate=predicates[j];
-			let object=this.rdf[subject][predicate];
-			if(Array.isArray(object))for(let k=0;k<object.length;k++)tbody.append($("<tr/>").append($("<td/>").text(subject)).append($("<td/>").text(predicate)).append($("<td/>").text(object[k])));
-			else tbody.append($("<tr/>").append($("<td/>").text(subject)).append($("<td/>").text(predicate)).append($("<td/>").text(object)));
+			let predicate2=predicates[j];
+			if(this._exclude(predicate,predicate2))continue;
+			let object2=this.rdf[subject2][predicate2];
+			if(Array.isArray(object2)){
+				for(let k=0;k<object2.length;k++){
+					if(this._exclude(object,object2[k]))continue;
+					rdf.add(subject2,predicate2,object2[k]);
+				}
+			}else{
+				if(this._exclude(object,object2))continue;
+				rdf.add(subject2,predicate2,object2);
+			}
 		}
 	}
-	return html;
+	this.mode=0;
+	return rdf;
 }
-RDF.prototype.toHTMLTable=function(labels){
-	let html=$("<table/>");
-	let thead=$("<thead/>");
-	let tr=$("<tr/>");
-	for(let i=0;i<labels.length;i++)tr.append($("<th/>").text(labels[i]));
-	thead.append(tr);
-	html.append(thead);
-	let tbody=$("<tbody/>");
-	html.append(tbody);
-	let hash={};
-	for(let i=0;i<labels.length;i++){
-		let label=labels[i];
-		let array=this.query("$key->"+label+"->$value");
-		for(let j=0;j<array.length;j++){
-			let h=array[j];
-			let key=h["$key"];
-			let value=h["$value"];
-			if(!(key in hash))hash[key]={};
-			if(!(label in hash[key]))hash[key][label]=value;
-			else if(Array.isArray(hash[key]))hash[key][label].push(value);
-			else hash[key][label]=[hash[key][label],value];
-		}
+//#################### execute ####################
+RDF.prototype.execute=function(func){
+	let keys=this._getParams(func);
+	this.returns=[];
+	for(let i=0;i<this.results.length;i++){
+		let h=this.results[i];
+		let params=[];
+		for(let j=0;j<keys.length;j++)params[j]=h["$"+keys[j]];
+		this.returns.push(func.apply(null,params));
 	}
-	let keys=Object.keys(hash).sort();
-	for(let i=0;i<keys.length;i++){
-		let tr=$("<tr/>");
-		let h=hash[keys[i]];
-		for(let j=0;j<labels.length;j++)tr.append("<td>"+h[labels[j]]+"</td>");
-		tbody.append(tr);
-	}
-	return html;
+	this.mode=2;
+	return this;
 }
 //#################### insert ####################
 RDF.prototype.insert=function(){
@@ -89,36 +108,48 @@ RDF.prototype.insert=function(){
 			this.add(tokens[0],tokens[1],tokens[2]);
 		}
 	}
+	this.mode=0;
 	return this;
 }
 //#################### put ####################
 RDF.prototype.put=function(subject,predicate,object){
 	if(!(subject in this.rdf))this.rdf[subject]={};
 	this.rdf[subject][predicate]=object;
+	this.mode=0;
 }
 //#################### query ####################
 RDF.prototype.query=function(string){
 	if(string==null)return this;
 	if(!Array.isArray(string))string=string.split(",");
-	let array=[{}];
+	this.results=[{}];
 	for(let i=0;i<string.length;i++){
 		let line=string[i];
 		let tokens=line.split("->");
 		if(tokens.length!=3)continue;
 		let temp=[];
-		for(let j=0;j<array.length;j++){
-			let hash=array[j];
+		for(let j=0;j<this.results.length;j++){
+			let hash=this.results[j];
 			let values=[];
 			let subject=this._queryvalue(tokens[0],hash);
 			let predicate=this._queryvalue(tokens[1],hash);
 			let object=this._queryvalue(tokens[2],hash);
 			let rdf=this.select(subject,predicate,object);
 			let variables=rdf._getVariables(tokens[0],tokens[1],tokens[2],hash);
-			for(let k=0;k<variables.length;k++)temp.push(variables[k]);
+			for(let k=0;k<variables.length;k++){
+				let keys=Object.keys(variables[k]);
+				for(let l=0;l<keys.length;l++){
+					let key=keys[l];
+					let value=variables[k][key];
+					if(!isNaN(value))value=Number(value);
+					variables[k][key]=value;
+				}
+				temp.push(variables[k]);
+			}
 		}
-		array=temp;
+		this.results=temp;
 	}
-	return array;
+	this.mode=1;
+	return this;
 }
 //#################### read ####################
 RDF.prototype.readTable=function(text){
@@ -135,6 +166,7 @@ RDF.prototype.readTable=function(text){
 			if(predicate!="")this.add(subject,predicate,object);
 		}
 	}
+	this.mode=0;
 	return this;
 }
 RDF.prototype.readTriple=function(text){
@@ -145,32 +177,8 @@ RDF.prototype.readTriple=function(text){
 		if(tokens.length!=3)continue;
 		this.add(tokens[0],tokens[1],tokens[2]);
 	}
+	this.mode=0;
 	return this;
-}
-//#################### remove ####################
-RDF.prototype.remove=function(subject,predicate,object){
-	let rdf=new RDF();
-	let subjects=Object.keys(this.rdf);
-	for(let i=0;i<subjects.length;i++){
-		let subject2=subjects[i];
-		if(this._removesub(subject,subject2))continue;
-		let predicates=Object.keys(this.rdf[subject2]);
-		for(let j=0;j<predicates.length;j++){
-			let predicate2=predicates[j];
-			if(this._removesub(predicate,predicate2))continue;
-			let object2=this.rdf[subject2][predicate2];
-			if(Array.isArray(object2)){
-				for(let k=0;k<object2.length;k++){
-					if(this._removesub(object,object2[k]))continue;
-					rdf.add(subject2,predicate2,object2[k]);
-				}
-			}else{
-				if(this._removesub(object,object2))continue;
-				rdf.add(subject2,predicate2,object2);
-			}
-		}
-	}
-	return rdf;
 }
 //#################### select ####################
 RDF.prototype.select=function(subject,predicate,object){
@@ -178,40 +186,145 @@ RDF.prototype.select=function(subject,predicate,object){
 	let subjects=Object.keys(this.rdf);
 	for(let i=0;i<subjects.length;i++){
 		let subject2=subjects[i];
-		if(this._selectsub(subject,subject2))continue;
+		if(this._select(subject,subject2))continue;
 		let predicates=Object.keys(this.rdf[subject2]);
 		for(let j=0;j<predicates.length;j++){
 			let predicate2=predicates[j];
-			if(this._selectsub(predicate,predicate2))continue;
+			if(this._select(predicate,predicate2))continue;
 			let object2=this.rdf[subject2][predicate2];
 			if(Array.isArray(object2)){
 				for(let k=0;k<object2.length;k++){
-					if(this._selectsub(object,object2[k]))continue;
+					if(this._select(object,object2[k]))continue;
 					rdf.add(subject2,predicate2,object2[k]);
 				}
 			}else{
-				if(this._selectsub(object,object2))continue;
+				if(this._select(object,object2))continue;
 				rdf.add(subject2,predicate2,object2);
 			}
 		}
 	}
+	this.mode=0;
 	return rdf;
 }
-//#################### text ####################
-RDF.prototype.toText=function(){
-	let array=[];
-	let subjects=Object.keys(this.rdf);
-	for(let i=0;i<subjects.length;i++){
-		let subject=subjects[i];
-		let predicates=Object.keys(this.rdf[subject]);
-		for(let j=0;j<predicates.length;j++){
-			let predicate=predicates[j];
-			let object=this.rdf[subject][predicate];
-			if(Array.isArray(object)){for(let k=0;k<object.length;k++){array.push(subject+"\t"+predicate+"\t"+object[k]);}}
-			else{array.push(subject+"\t"+predicate+"\t"+object);}
+//#################### stringify ####################
+RDF.prototype.stringify=function(){
+	if(this.mode==0)return JSON.stringify(this.rdf);
+	else return JSON.stringify(this.results);
+}
+//#################### toHTML ####################
+RDF.prototype.toHTML=function(){
+	if(this.mode==0){
+		let html=$("<table/>");
+			let thead=$("<thead/>").append($("<tr/>").append($("<th>").text("subject")).append($("<th>").text("predicate")).append($("<th>").text("object")));
+		html.append(thead);
+		let tbody=$("<tbody/>");
+		html.append(tbody);
+		let subjects=Object.keys(this.rdf);
+		for(let i=0;i<subjects.length;i++){
+			let subject=subjects[i];
+			let predicates=Object.keys(this.rdf[subject]);
+			for(let j=0;j<predicates.length;j++){
+				let predicate=predicates[j];
+				let object=this.rdf[subject][predicate];
+				if(Array.isArray(object))for(let k=0;k<object.length;k++)tbody.append($("<tr/>").append($("<td/>").text(subject)).append($("<td/>").text(predicate)).append($("<td/>").text(object[k])));
+				else tbody.append($("<tr/>").append($("<td/>").text(subject)).append($("<td/>").text(predicate)).append($("<td/>").text(object)));
+			}
 		}
+		return html;
+	}else if(this.mode==1){
+		let keys=this._keys();
+		let html=$("<table/>");
+		let thead=$("<thead/>").append($("<tr/>"));
+		for(let i=0;i<keys.length;i++)thead.append($("<th/>").text(keys[i]));
+		html.append(thead);
+		let tbody=$("<tbody/>");
+		for(let i=0;i<this.results.length;i++){
+			let tr=tbody.append($("<tr/>"));
+			for(let j=0;j<keys.length;j++)tr.append($("<td/>").text(this.results[i][keys[j]]));
+		}
+		html.append(tbody);
+		return html;
+	}else{
+		let keys=this._keys();
+		let html=$("<table/>");
+		let thead=$("<thead/>").append($("<tr/>"));
+		for(let i=0;i<keys.length;i++)thead.append($("<th/>").text(keys[i]));
+		thead.append($("<th/>").text("return"));
+		html.append(thead);
+		let tbody=$("<tbody/>");
+		for(let i=0;i<this.returns.length;i++){
+			let tr=tbody.append($("<tr/>"));
+			for(let j=0;j<keys.length;j++)tr.append($("<td/>").text(this.results[i][keys[j]]));
+			tr.append($("<td/>").text(this.returns[i]));
+		}
+		html.append(tbody);
+		return html;
 	}
-	return array.join("\n");
+}
+RDF.prototype.toHTMLTable=function(labels){
+	if(this.mode==0){
+		let html=$("<table/>");
+		let thead=$("<thead/>");
+		let tr=$("<tr/>");
+		for(let i=0;i<labels.length;i++)tr.append($("<th/>").text(labels[i]));
+		thead.append(tr);
+		html.append(thead);
+		let tbody=$("<tbody/>");
+		html.append(tbody);
+		let hash={};
+		for(let i=0;i<labels.length;i++){
+			let label=labels[i];
+			let array=this.query("$key->"+label+"->$value");
+			for(let j=0;j<array.length;j++){
+				let h=array[j];
+				let key=h["$key"];
+				let value=h["$value"];
+				if(!(key in hash))hash[key]={};
+				if(!(label in hash[key]))hash[key][label]=value;
+				else if(Array.isArray(hash[key]))hash[key][label].push(value);
+				else hash[key][label]=[hash[key][label],value];
+			}
+		}
+		let keys=Object.keys(hash).sort();
+		for(let i=0;i<keys.length;i++){
+			let tr=$("<tr/>");
+			let h=hash[keys[i]];
+			for(let j=0;j<labels.length;j++)tr.append("<td>"+h[labels[j]]+"</td>");
+			tbody.append(tr);
+		}
+		return html;
+	}
+}
+//#################### toText ####################
+RDF.prototype.toText=function(string){
+	if(this.mode==0){
+		let array=[];
+		let subjects=Object.keys(this.rdf);
+		for(let i=0;i<subjects.length;i++){
+			let subject=subjects[i];
+			let predicates=Object.keys(this.rdf[subject]);
+			for(let j=0;j<predicates.length;j++){
+				let predicate=predicates[j];
+				let object=this.rdf[subject][predicate];
+				if(Array.isArray(object)){for(let k=0;k<object.length;k++){array.push(subject+"\t"+predicate+"\t"+object[k]);}}
+				else{array.push(subject+"\t"+predicate+"\t"+object);}
+			}
+		}
+		return array.join("\n");
+	}else{
+		let keys=this._keys();
+		let lines=[];
+		for(let i=0;i<this.results.length;i++){
+			let line=string;
+			for(let j=0;j<keys.length;j++){
+				let key=keys[j]
+				let val=this.results[i][key];
+				line=line.replace(key,val);
+			}
+			lines.push(line);
+		}
+		return lines;
+	}
 }
 //#################### triples ####################
 RDF.prototype.subjects=function(){return Object.keys(this.rdf);}
@@ -261,6 +374,7 @@ RDF.prototype._getVariables=function(subject,predicate,object,hash){
 				if(subject.startsWith("$"))temp[subject]=subject2;
 				if(predicate.startsWith("$"))temp[predicate]=predicate2;
 				if(object.startsWith("$"))temp[object]=object2[k];
+				if(Object.keys(temp).length==0){continue;}
 				if(!this._includes(array,temp))array.push(temp);
 			}
 		}
@@ -282,13 +396,15 @@ RDF.prototype._equals=function(hash1,hash2){
 	return 1;
 }
 RDF.prototype._queryvalue=function(value,hash){
-	if(value==null||value==""){return null;}
+	if(value==null||value==""){return new RegExp(".*");}
 	else if(value.startsWith("$")){if(value in hash)return hash[value];else return value;}
-	return new RegExp(value);
+	else if(value.startsWith("/")&&value.endsWith("/"))return new RegExp(value.substr(1,value.length-2));
+	else return value;
 }
-RDF.prototype._selectsub=function(value,value2){
+RDF.prototype._select=function(value,value2){
+	if(!isNaN(value))value=String(value);
 	if(value==null)return 1;
-	else if(Array.isArray(value)){for(let i=0;i<value.length;i++){if(this._selectsub(value[i],value2)==0)return 0;}return 1;}
+	else if(Array.isArray(value)){for(let i=0;i<value.length;i++){if(this._select(value[i],value2)==0)return 0;}return 1;}
 	else if(value instanceof RegExp)return !value2.match(value);
 	else if(value.startsWith("$"))return 0;
 	else if(value.startsWith("!")){if(value.substr(1)==value2)return 1;}
@@ -296,13 +412,35 @@ RDF.prototype._selectsub=function(value,value2){
 	else if(value!=value2)return 1;
 	else return 0;
 }
-RDF.prototype._removesub=function(value,value2){
+RDF.prototype._exclude=function(value,value2){
 	if(value==null)return 0;
-	else if(Array.isArray(value)){for(let i=0;i<value.length;i++){if(this._removesub(value[i],value2)==1)return 1;}return 0;}
+	else if(Array.isArray(value)){for(let i=0;i<value.length;i++){if(this._exclude(value[i],value2)==1)return 1;}return 0;}
 	else if(value instanceof RegExp)return value2.match(value);
 	else if(value.startsWith("$"))return 0;
 	else if(value.startsWith("!")){if(value.substr(1)==value2)return 0;}
 	else if(value==value2)return 1;
 	else if(value!=value2)return 0;
 	else return 1;
+}
+RDF.prototype._keys=function(){
+	let keys=[];
+	for(let i=0;i<this.results.length;i++){
+		let temp=Object.keys(this.results[i])
+		for(let j=0;j<temp.length;j++)if(!keys.includes(temp[j]))keys.push(temp[j]);
+	}
+	return keys;
+}
+RDF.prototype._getParams=function(func) {
+	var str=func.toString();
+	var flag=0;
+	var key="";
+	for(var i=0;i<str.length;i++){
+		if(str[i]=="("){flag=1;continue;}
+		if(flag!=1)continue;
+		if(str[i]==")")break;
+		key+=str[i];
+	}
+	let keys=key.split(",");
+	for(var i=0;i<keys.length;i++)keys[i]=keys[i].trim();
+	return keys;
 }
